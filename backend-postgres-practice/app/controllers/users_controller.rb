835dcf5ad50f69b3a6294cce10b4ebfde9425975
OM Miles <miles@ommiles.com>
2021-07-23 08:38:12 -0700

    update application_controller

--- backend-postgres-practice/app/controllers/users_controller.rb
+++ backend-postgres-practice/app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+    before_action :authorized, only: [:index, :update, :destroy]
+    skip_before_action :authorized, only: [:show, :login]
+    def index
+        users = User.all
+        render json: users, except: [:password_digest]
+    end
+
+    def show
+        user = User.find_by(:id)
+        render json: user
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            token = encode_token({ user_id: user.id })
+            render json: { user: user, token: token }, status: :accepted
+        else
+            render json: { errors: user.errors.full_messages }, status: :unauthorized
+        end
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(edit_params)
+        if user.valid?
+            token = encode_token({ user_id: user.id })
+            render json: { user: user, token: token }, status: :accepted
+        else
+            render json: { errors: user.errors.full_messages }, status: :unauthorized
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password)
+    end
+
+    def edit_params
+        params.permit(:username, :password)
+    end
 end
--- backend-postgres-practice/app/models/user.rb
+++ backend-postgres-practice/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    # This method provides us with a number of methods...
+    # ... to set up and authenticate user passwords with bcrypt.
+    validates :username, uniqueness: true
+    has_secure_password
 end
