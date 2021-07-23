7b698e7a01f90738a324c696264d7a18d374fd41
OM Miles <miles@ommiles.com>
2021-07-23 12:19:38 -0700

    add front end

--- backend-postgres-practice/app/controllers/application_controller.rb
+++ backend-postgres-practice/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
     SECRET = Rails.application.credentials.dig(:secret_key_base)
+    # Put the app on lockdown!
     before_action :authorized
 
     # This action will receive a user’s username... 
@@ -42,5 +43,58 @@ class ApplicationController < ActionController::API
 
     def login_params
         params.permit(:username, :password)
+    end
+
+    def encode_token(payload)
+        # Using the JWT gem, the encode method takes the payload { username: 'abc' }
+        # and our encrypted key(string) as arguments.
+        # The jwt will be returned.
+        JWT.encode(payload, SECRET)
+    end
+
+    def auth_header
+        # When we fetch from the client, the token is in the request headers.
+        # Retrieve the key 'Authorization' and return in our auth_header helper method
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        # If there's no auth header in an incoming request...
+        # or if auth_header's return value is nil...
+        # ... don't run the method below.
+        if auth_header 
+            token = auth_header.split(' ')[1]
+            begin
+                # Hash can only be decoded with the correct SECRET and token.
+                # Once decoded, we are returned an array of 2 objects:
+                # Header object (has algorithm type) and payload object (has user's info).
+                JWT.decode(token, SECRET, true, algorithm: "HS256")
+            rescue JWT::DecodeError
+                # If there's an error, return nil
+                nil
+            end
+        end   
+    end
+
+    def current_user
+        # If decoded token was not nil,
+        # retrieve the user_id from the payload we sent with the fetch.
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            # Lookup that user ...
+            user = User.find_by(id: user_id)
+        end
+    end
+
+    def logged_in?
+        # Coerce the current_user value to a boolean...
+        # ... depending on the presence of a current user.
+        !!current_user
+    end
+
+    def authorized
+        # This is our gatekeeper helper method.
+        # If we call authorize method and the user is not logged in, return the below message in JSON to the client.
+        render json: {message: "Please log in"}, status: :unauthorized unless logged_in?
     end
 end
