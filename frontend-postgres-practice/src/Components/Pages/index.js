260d6d7185464d2f0e7f8e6d59c7506543b763f9
OM Miles <miles@ommiles.com>
2021-07-26 16:00:10 -0700

    pages set up

---
+++ frontend-postgres-practice/src/Components/Pages/index.js
@@ -0,0 +1,11 @@
+import { Signup } from './Signup';
+import { Login } from './Login';
+import { Home } from './Home';
+
+const Pages = {
+  Signup,
+  Login,
+  Home,
+};
+
+export default Pages;
---
+++ frontend-postgres-practice/src/Components/Routes/Routes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Pages from '../Pages'
+
+export const Routes = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
